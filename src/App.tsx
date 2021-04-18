4592667630b2fe196a706d781b6715e94d3ef41d
Alexandre Consigny <alexandre.consigny@gmail.com>
2021-04-17 20:12:25 -0400

    add react and hot reload

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { hot } from 'react-hot-loader';
+import * as React from 'react';
+
+const App = () => {
+  return (
+    <h2>Hello from React!</h2>
+  );
+}
+
+export default hot(module)(App);
